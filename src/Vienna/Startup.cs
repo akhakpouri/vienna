907c563bc44b2ce30fdb6aaacfed10ef06c2b017
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-07-13 16:22:18 -0400

    changed the app name, upgraded it to the latest version of ng

--- src/Vienna/Startup.cs
+++ src/Vienna/Startup.cs
@@ -1,10 +1,12 @@
 using Daytona.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
 
 namespace Vienna
 {
@@ -19,12 +21,17 @@ namespace Vienna
         public IConfiguration Configuration { get; }
         public IHostEnvironment Environment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
             services.AddUseFriendlyExceptionsMiddleware();
+
+            if (!Environment.IsDevelopment())
+            {
+                var redis = ConnectionMultiplexer.Connect(Configuration["Redis:Uri"]);
+                services.AddDataProtection().PersistKeysToStackExchangeRedis(redis);
+            }
 
             services.AddSpaStaticFiles(configuration =>
             {
@@ -42,7 +49,6 @@ namespace Vienna
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
