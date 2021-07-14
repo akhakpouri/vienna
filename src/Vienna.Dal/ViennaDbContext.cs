2026a472f5e14c9163e81b0775795ab144d7e814
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-07-14 16:12:50 -0400

    Added a new email message model

---
+++ src/Vienna.Dal/ViennaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Vienna.Dal.Models;
+
+namespace Vienna.Dal
+{
+    public class ViennaDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ViennaDbContext(DbContextOptions<ViennaDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(b =>
+            {
+                b.HasMany(u => u.Claims)
+                .WithOne()
+                .HasForeignKey(uc => uc.UserId)
+                .IsRequired();
+            });
+        }
+    }
+}
